a85447aa49ce67b71650139a5d293e040755b915
Siniša <sinisa.stanic@octomind.dev>
2023-05-23 16:49:08 +0200

    I hate commander

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import {chromium} from 'playwright'
 import ngrok from 'ngrok'
-import {program} from 'commander'
-
-program.option('--port')
-program.parse()
-program.help()
+import {Command} from 'commander'
 
 const main = async (): Promise<void> => {
-  const port = 8888
+  const program = new Command()
+
+  program
+    .option(
+      '-p, --port <number>',
+      'port on which chromium will be launched',
+      element => parseInt(element)
+    )
+    // eslint-disable-next-line no-undef
+    .parse(process.argv)
+
+  const options = program.opts()
+
+  const port = options.port ?? 8888
   const server = await chromium.launchServer({
     devtools: true,
     port
@@ -19,8 +28,9 @@ const main = async (): Promise<void> => {
     .wsEndpoint()
     .replace(`ws://127.0.0.1:${port}`, `wss://${url.replace('https://', '')}`)
 
+  // eslint-disable-next-line no-undef,no-console
   console.log(
-    `you can pass "${reverseProxyPlaywrightWsEndpoint}" as wsEndpoint to /execute in order to run automagically against local environment`
+    `you can pass "${reverseProxyPlaywrightWsEndpoint}" as the wsEndpoint to /execute in order to run automagically against your local environment`
   )
 }
 
