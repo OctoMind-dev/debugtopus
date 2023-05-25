914fe8425f1846576c1efdd0b114ec3fb65bdfa4
Siniša <sinisa.stanic@octomind.dev>
2023-05-25 11:13:00 +0200

    modern problems require modern solutions

--- tests/debugtopus.spec.ts
+++ tests/debugtopus.spec.ts
@@ -19,7 +19,7 @@ describe("debugtopus", () => {
     //don't leave hanging process in failure cases
     const timer = setTimeout(() => {
       killEntireProcessGroup(child);
-    }, 10_000);
+    }, 100_000);
 
     for await (const data of child.stdout) {
       const regex = /"(?<url>wss:\/\/.*)"/;
